b28539abe1b1d67d1f6f22ce80bc33c98503cd68
kennethdavidlopezgsanjosemlg-ux <kennethdavilopezg.sanjosemlg@fundacionloyola.net>
2026-03-27 08:31:24 +0100

    commit igualar clase empleado en las dos ramas

--- src/modelo/Empleado.java
+++ src/modelo/Empleado.java
@@ -2,58 +2,57 @@ package modelo;
 
 public abstract class Empleado {
 
-    private static final int SALARIO_MINIMO_INTERPROFESIONAL = 1221;
+	private static final int SALARIO_MINIMO_INTERPROFESIONAL = 1221;
 	protected String nombre;
-    protected double salarioBase;
-    protected boolean autonomo;
-    protected float irpf;
+	protected double salarioBase;
+	protected boolean autonomo;
+	protected float irpf;
 
 	public Empleado(String nombre, double salarioBase, Boolean autonomo, float irpf) {
-        if (salarioBase < SALARIO_MINIMO_INTERPROFESIONAL) {
-            throw new IllegalArgumentException("El salario base no puede ser negativo");
-        }
-        this.nombre = nombre;
-        this.salarioBase = salarioBase;
-        this.autonomo = autonomo;
-        this.irpf=irpf;
-    }
-	
-	public Empleado(String nombre) {
-        this.nombre = nombre;
-        this.salarioBase = SALARIO_MINIMO_INTERPROFESIONAL;
-        this.autonomo = false;
-        this.irpf=0;
-    }
-	
-	// Métodos
-	
-	public double calcularSalarioBruto(double pagaExtra, int mesesProrratea) {
-		
-		return (this.salarioBase + pagaExtra) * mesesProrratea;
-		
+		if (salarioBase < SALARIO_MINIMO_INTERPROFESIONAL) {
+			throw new IllegalArgumentException("El salario base no puede ser negativo");
+		}
+		this.nombre = nombre;
+		this.salarioBase = salarioBase;
+		this.autonomo = autonomo;
+		this.irpf = irpf;
 	}
-	
-	public abstract boolean calcularSalarioNeto();
-	
+
+	public Empleado(String nombre) {
+		this.nombre = nombre;
+		this.salarioBase = SALARIO_MINIMO_INTERPROFESIONAL;
+		this.autonomo = false;
+		this.irpf = 0;
+	}
+
+	// Métodos
+
+	public double calcularSalarioBruto(double pagaExtra, int mesesProrratea) {
+		return (this.salarioBase + pagaExtra) * mesesProrratea;
+
+	}
+
+	public abstract double calcularSalarioNeto();
+
 	// Getters y Setters
-	
-    public String getNombre() {
-        return nombre;
-    }
 
-    public double getSalarioBase() {
-        return salarioBase;
-    }
-    
-    public boolean isAutonomo() {
-    	return autonomo;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public float getIrpf() {
+	public double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public boolean isAutonomo() {
+		return autonomo;
+	}
+
+	public float getIrpf() {
 		return irpf;
 	}
-    
-    public void setNombre(String nombre) {
+
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
@@ -68,5 +67,5 @@ public abstract class Empleado {
 	public void setIrpf(float irpf) {
 		this.irpf = irpf;
 	}
-	
+
 }
